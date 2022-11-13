a36714409ebca0d6a20ed7b6de55160e5475a541
tison.yin <yinqizhen@aliyun.com>
2022-11-13 22:34:17 +0800

    update

--- router/router.go
+++ router/router.go
@@ -56,11 +56,17 @@ func (ar *AppRouter) InitRouter() {
 	ar.addWSRouter()
 	ar.addMaterialRouter()
 	ar.addDepartmentRouter()
+	ar.addWechatApiRouter()
 }
 
 // see api docs on http://localhost:5001/swagger/index.html
+
 func (ar *AppRouter) addSwaggerRouter() {
 	ar.router.GET("/swagger/*any", gs.WrapHandler(swaggerFiles.Handler))
+}
+
+func (ar *AppRouter) addWechatApiRouter() {
+	ar.router.POST("/", ar.handler.WechatReceive)
 }
 
 func (ar *AppRouter) addLoginRouter() {
